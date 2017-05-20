22560c168f4727c685df554ae5062c182f8b9899
arun reddy <arun reddy>
2017-05-20 13:20:15 -0500

    setup app for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
 
 var app=express();
+const port=process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 app.post('/todos',(req,res)=>{
     var todo=new Todo({
@@ -33,7 +35,7 @@ app.get('/todos/:id',(req,res)=>{
     {
         return res.status(404).send();
     }
-    
+
     Todo.findById(id).then((todo)=>{
         if(!todo){
             return res.status(404).send();
@@ -44,8 +46,8 @@ app.get('/todos/:id',(req,res)=>{
         res.status(400).send();
     });
 });
-app.listen(3000,()=>{
-    console.log('TodoApp server has started listening');
+app.listen(port,()=>{
+    console.log(`TodoApp server has started listening on ${port}`);
 });
 
 module.exports={app};
